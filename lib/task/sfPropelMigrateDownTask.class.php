eb2130a31c8e91d61f218efc0814b9eceda29e58
Marko Ognjenovic <marko.ognjenovicgm@gmail.com>
2021-09-08 11:46:04 +0200

    Reimplement sfPropelMigrateDownTask

--- lib/task/sfPropelMigrateDownTask.class.php
+++ lib/task/sfPropelMigrateDownTask.class.php
@@ -18,140 +18,123 @@
  */
 class sfPropelMigrateDownTask extends sfPropelBaseTask
 {
-  private const OK = 0;
-  private const NOT_OK = 1;
+    private const OK = 0;
+    private const NOT_OK = 1;
 
-  /**
-   * @see sfTask
-   */
-  protected function configure()
-  {
-    $this->addOptions(array(
-      new sfCommandOption('application', null, sfCommandOption::PARAMETER_OPTIONAL, 'The application name', true),
-      new sfCommandOption('env', null, sfCommandOption::PARAMETER_REQUIRED, 'The environment', 'cli'),
-      new sfCommandOption('migration-dir', null, sfCommandOption::PARAMETER_OPTIONAL, 'The migrations subdirectory', 'lib/model/migration'),
-      new sfCommandOption('migration-table', null, sfCommandOption::PARAMETER_OPTIONAL, 'The name of the migration table', 'propel_migration'),
-      new sfCommandOption('verbose', null, sfCommandOption::PARAMETER_NONE, 'Enables verbose output'),
-    ));
-    $this->namespace = 'propel';
-    $this->name = 'down';
-    $this->aliases = array('migration-down');
-    $this->briefDescription = 'Executes the next migration down';
+    /**
+     * @see sfTask
+     */
+    protected function configure()
+    {
+        $this->addOptions(array(
+            new sfCommandOption('application', null, sfCommandOption::PARAMETER_OPTIONAL, 'The application name', true),
+            new sfCommandOption('env', null, sfCommandOption::PARAMETER_REQUIRED, 'The environment', 'cli'),
+            new sfCommandOption('migration-dir', null, sfCommandOption::PARAMETER_OPTIONAL, 'The migrations subdirectory', 'lib/model/migration'),
+            new sfCommandOption('migration-table', null, sfCommandOption::PARAMETER_OPTIONAL, 'The name of the migration table', 'migration'),
+            new sfCommandOption('verbose', null, sfCommandOption::PARAMETER_NONE, 'Enables verbose output'),
+        ));
+        $this->namespace = 'propel';
+        $this->name = 'down';
+        $this->aliases = array('migration-down');
+        $this->briefDescription = 'Executes the next migration down';
 
-    $this->detailedDescription = <<<EOF
+        $this->detailedDescription = <<<EOF
 The [propel:up|INFO] checks the version of the database structure, and looks for migration files already executed (i.e. with a lower version timestamp). The last executed migration found is reversed.
 
 The task reads the database connection settings in [config/databases.yml|COMMENT].
 
 The task looks for migration classes in [lib/model/migration|COMMENT].
 EOF;
-  }
-
-  /**
-   * @see sfTask
-   */
-  protected function execute($arguments = array(), $options = array())
-  {
-    $databaseManager = new sfDatabaseManager($this->configuration);
-    $connections = $this->getConnections($databaseManager);
-    $manager = new PropelMigrationManager();
-    $manager->setConnections($connections);
-    $manager->setMigrationTable($options['migration-table']);
-    $migrationDirectory = sfConfig::get('sf_root_dir') . DIRECTORY_SEPARATOR . $options['migration-dir'];
-    $manager->setMigrationDir($migrationDirectory);
-
-    $previousTimestamps = $manager->getAlreadyExecutedMigrationTimestamps();
-    if (!$nextMigrationTimestamp = array_pop($previousTimestamps))
-    {
-      $this->logSection('propel', 'No migration were ever executed on this database - nothing to reverse.');
-      return self::OK;
-    }
-    $this->logSection('propel', sprintf(
-      'Executing migration %s down',
-      $manager->getMigrationClassName($nextMigrationTimestamp)
-    ));
-
-    if ($nbPreviousTimestamps = count($previousTimestamps))
-    {
-      $previousTimestamp = array_pop($previousTimestamps);
-    }
-    else
-    {
-      $previousTimestamp = 0;
     }
 
-    $migration = $manager->getMigrationObject($nextMigrationTimestamp);
-    if (false === $migration->preDown($manager))
+    /**
+     * @see sfTask
+     */
+    protected function execute($arguments = array(), $options = array())
     {
-      $this->logSection('propel', 'preDown() returned false. Aborting migration.', null, 'ERROR');
-      return self::NOT_OK;
-    }
+        $databaseManager = new sfDatabaseManager($this->configuration);
+        $connections = $this->getConnections($databaseManager);
+        $manager = new sfPropelMigrationManager();
+        $manager->setConnections($connections);
+        $manager->setMigrationTable($options['migration-table']);
+        $migrationDirectory = sfConfig::get('sf_root_dir') . DIRECTORY_SEPARATOR . $options['migration-dir'];
+        $manager->setMigrationDir($migrationDirectory);
 
-    foreach ($migration->getDownSQL() as $datasource => $sql)
-    {
-      $connection = $manager->getConnection($datasource);
-      if ($options['verbose'])
-      {
-        $this->logSection('propel', sprintf('  Connecting to database "%s" using DSN "%s"', $datasource, $connection['dsn']), null, 'COMMENT');
-      }
-      $pdo = $manager->getPdoConnection($datasource);
-      $res = 0;
-      $statements = PropelSQLParser::parseString($sql);
-      foreach ($statements as $statement)
-      {
-        try
+        $latestExecutedMigrations = $manager->getExecutedMigrationNamesForBatch($manager->getLatestBatch());
+        if (empty($latestExecutedMigrations))
         {
-          if ($options['verbose'])
-          {
-            $this->logSection('propel', sprintf('  Executing statement "%s"', $statement), null, 'COMMENT');
-          }
-          $stmt = $pdo->prepare($statement);
-          $stmt->execute();
-          $res++;
+            $this->logSection('propel', 'No migration were ever executed on this database - nothing to reverse.');
+            return self::OK;
         }
-        catch (PDOException $e)
-        {
-          $this->logSection(sprintf('Failed to execute SQL "%s". Aborting migration.', $statement), null, 'ERROR');
-          return self::NOT_OK;
-          // continue
+        $this->logSection('propel', 'Executing migrations down');
+
+        foreach ($latestExecutedMigrations as $migrationName) {
+            $migration = $manager->getMigrationObject($migrationName);
+            if (false === $migration->preDown($manager)) {
+                $this->logSection('propel', 'preDown() returned false. Aborting migration.', null, 'ERROR');
+
+                return self::NOT_OK;
+            }
+
+            foreach ($migration->getDownSQL() as $datasource => $sql) {
+                $connection = $manager->getConnection($datasource);
+                if ($options['verbose']) {
+                    $this->logSection('propel',
+                        sprintf('  Connecting to database "%s" using DSN "%s"', $datasource, $connection['dsn']), null,
+                        'COMMENT');
+                }
+                $pdo = $manager->getPdoConnection($datasource);
+                $res = 0;
+                $statements = PropelSQLParser::parseString($sql);
+                foreach ($statements as $statement) {
+                    try {
+                        if ($options['verbose']) {
+                            $this->logSection('propel', sprintf('  Executing statement "%s"', $statement), null, 'COMMENT');
+                        }
+                        $stmt = $pdo->prepare($statement);
+                        $stmt->execute();
+                        $res++;
+                    } catch (PDOException $e) {
+                        $this->logSection(sprintf('Failed to execute SQL "%s". Aborting migration.', $statement), null,
+                            'ERROR');
+
+                        return self::NOT_OK;
+                        // continue
+                    }
+                }
+                if (! $res) {
+                    $this->logSection('propel', sprintf(
+                        'Please review the code in "%s"',
+                        $manager->getMigrationDir() . DIRECTORY_SEPARATOR . $migrationName
+                    ));
+                    $this->logSection('propel', 'Migration aborted', null, 'ERROR');
+
+                    return self::NOT_OK;
+                }
+                $this->logSection('propel', sprintf(
+                    '%d of %d SQL statements executed successfully on datasource "%s"',
+                    $res,
+                    count($statements),
+                    $datasource
+                ));
+
+                $manager->removeExecutedMigration($datasource, $migrationName);
+                if ($options['verbose']) {
+                    $this->logSection(
+                        'propel',
+                        sprintf('  Removed %s from executed migrations for datasource "%s"', $migrationName, $datasource),
+                        null,
+                        'COMMENT'
+                    );
+                }
+            }
+
+            $migration->postDown($manager);
         }
-      }
-      if (!$res)
-      {
-        $this->logSection('propel', 'No statement was executed. The version was not updated.');
-        $this->logSection('propel', sprintf(
-          'Please review the code in "%s"',
-          $manager->getMigrationDir() . DIRECTORY_SEPARATOR . $manager->getMigrationClassName($nextMigrationTimestamp)
-        ));
-        $this->logSection('propel', 'Migration aborted', null, 'ERROR');
-        return self::NOT_OK;
-      }
-      $this->logSection('propel', sprintf(
-        '%d of %d SQL statements executed successfully on datasource "%s"',
-        $res,
-        count($statements),
-        $datasource
-      ));
 
-      $manager->updateLatestMigrationTimestamp($datasource, $previousTimestamp);
-      if ($options['verbose'])
-      {
-        $this->logSection('propel', sprintf('  Downgraded latest migration date to %d for datasource "%s"', $previousTimestamp, $datasource), null, 'COMMENT');
-      }
+        $this->logSection('propel', sprintf('Reverse migration complete. %d migrations were rolled back.', count($latestExecutedMigrations)));
+
+        return self::OK;
     }
-
-    $migration->postDown($manager);
-
-    if ($nbPreviousTimestamps)
-    {
-      $this->logSection('propel', sprintf('Reverse migration complete. %d more migrations available for reverse.', $nbPreviousTimestamps));
-    }
-    else
-    {
-      $this->logSection('propel', 'Reverse migration complete. No more migration available for reverse');
-    }
-
-    return self::OK;
-  }
 
 }
