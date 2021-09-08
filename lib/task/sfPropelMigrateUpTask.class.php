0dc7e91f77854d992d31b018de0017a8522c283c
Marko Ognjenovic <marko.ognjenovicgm@gmail.com>
2021-09-08 11:45:36 +0200

    Reimplement sfPropelMigrateUpTask

--- lib/task/sfPropelMigrateUpTask.class.php
+++ lib/task/sfPropelMigrateUpTask.class.php
@@ -18,132 +18,137 @@
  */
 class sfPropelMigrateUpTask extends sfPropelBaseTask
 {
-  private const OK = 0;
-  private const NOT_OK = 1;
+    private const OK = 0;
+    private const NOT_OK = 1;
 
-  /**
-   * @see sfTask
-   */
-  protected function configure()
-  {
-    $this->addOptions(array(
-      new sfCommandOption('application', null, sfCommandOption::PARAMETER_OPTIONAL, 'The application name', true),
-      new sfCommandOption('env', null, sfCommandOption::PARAMETER_REQUIRED, 'The environment', 'cli'),
-      new sfCommandOption('migration-dir', null, sfCommandOption::PARAMETER_OPTIONAL, 'The migrations subdirectory', 'lib/model/migration'),
-      new sfCommandOption('migration-table', null, sfCommandOption::PARAMETER_OPTIONAL, 'The name of the migration table', 'propel_migration'),
-      new sfCommandOption('verbose', null, sfCommandOption::PARAMETER_NONE, 'Enables verbose output'),
-    ));
-    $this->namespace = 'propel';
-    $this->name = 'up';
-    $this->aliases = array('migration-up');
-    $this->briefDescription = 'Executes the next migration up';
+    /**
+     * @see sfTask
+     */
+    protected function configure()
+    {
+        $this->addOptions(array(
+            new sfCommandOption('application', null, sfCommandOption::PARAMETER_OPTIONAL, 'The application name', true),
+            new sfCommandOption('env', null, sfCommandOption::PARAMETER_REQUIRED, 'The environment', 'cli'),
+            new sfCommandOption('migration-dir', null, sfCommandOption::PARAMETER_OPTIONAL, 'The migrations subdirectory', 'lib/model/migration'),
+            new sfCommandOption('migration-table', null, sfCommandOption::PARAMETER_OPTIONAL, 'The name of the migration table', 'migration'),
+            new sfCommandOption('verbose', null, sfCommandOption::PARAMETER_NONE, 'Enables verbose output'),
+        ));
+        $this->namespace = 'propel';
+        $this->name = 'up';
+        $this->aliases = array('migration-up');
+        $this->briefDescription = 'Executes the next migration up';
 
-    $this->detailedDescription = <<<EOF
+        $this->detailedDescription = <<<EOF
 The [propel:up|INFO] checks the version of the database structure, and looks for migration files not yet executed (i.e. with a greater version timestamp). The first migration found is executed.
 
 The task reads the database connection settings in [config/databases.yml|COMMENT].
 
 The task looks for migration classes in [lib/model/migration|COMMENT].
 EOF;
-  }
-
-  /**
-   * @see sfTask
-   */
-  protected function execute($arguments = array(), $options = array())
-  {
-    $databaseManager = new sfDatabaseManager($this->configuration);
-    $connections = $this->getConnections($databaseManager);
-    $manager = new PropelMigrationManager();
-    $manager->setConnections($connections);
-    $manager->setMigrationTable($options['migration-table']);
-    $migrationDirectory = sfConfig::get('sf_root_dir') . DIRECTORY_SEPARATOR . $options['migration-dir'];
-    $manager->setMigrationDir($migrationDirectory);
-
-    if (!$nextMigrationTimestamp = $manager->getFirstUpMigrationTimestamp())
-    {
-      $this->logSection('propel', 'All migrations were already executed - nothing to migrate.');
-      return self::OK;
-    }
-    $this->logSection('propel', sprintf(
-      'Executing migration %s up',
-      $manager->getMigrationClassName($nextMigrationTimestamp)
-    ));
-
-    $migration = $manager->getMigrationObject($nextMigrationTimestamp);
-    if (false === $migration->preUp($manager))
-    {
-      $this->logSection('propel', 'preUp() returned false. Aborting migration.', null, 'ERROR');
-      return self::NOT_OK;
     }
 
-    foreach ($migration->getUpSQL() as $datasource => $sql)
+    /**
+     * @see sfTask
+     */
+    protected function execute($arguments = array(), $options = array())
     {
-      $connection = $manager->getConnection($datasource);
-      if ($options['verbose'])
-      {
-        $this->logSection('propel', sprintf('  Connecting to database "%s" using DSN "%s"', $datasource, $connection['dsn']), null, 'COMMENT');
-      }
-      $pdo = $manager->getPdoConnection($datasource);
-      $res = 0;
-      $statements = PropelSQLParser::parseString($sql);
-      foreach ($statements as $statement)
-      {
-        try
+        $databaseManager = new sfDatabaseManager($this->configuration);
+        $connections = $this->getConnections($databaseManager);
+        $manager = new sfPropelMigrationManager();
+        $manager->setConnections($connections);
+        $manager->setMigrationTable($options['migration-table']);
+        $migrationDirectory = sfConfig::get('sf_root_dir') . DIRECTORY_SEPARATOR . $options['migration-dir'];
+        $manager->setMigrationDir($migrationDirectory);
+
+        $missingMigrations = $manager->getMissingMigrationNames();
+
+        if (empty($missingMigrations))
         {
-          if ($options['verbose'])
-          {
-            $this->logSection('propel', sprintf('  Executing statement "%s"', $statement), null, 'COMMENT');
-          }
-          $stmt = $pdo->prepare($statement);
-          $stmt->execute();
-          $res++;
+            $this->logSection('propel', 'All migrations were already executed - nothing to migrate.');
+            return self::OK;
         }
-        catch (PDOException $e)
-        {
-          $this->logSection(sprintf('Failed to execute SQL "%s". Aborting migration.', $statement), null, 'ERROR');
-          return self::NOT_OK;
-          // continue
-        }
-      }
-      if (!$res)
-      {
-        $this->logSection('propel', 'No statement was executed. The version was not updated.');
+
+        $migrationName = array_shift($missingMigrations);
+
         $this->logSection('propel', sprintf(
-          'Please review the code in "%s"',
-          $manager->getMigrationDir() . DIRECTORY_SEPARATOR . $manager->getMigrationClassName($nextMigrationTimestamp)
+            'Executing migration %s up',
+            $migrationName
         ));
-        $this->logSection('propel', 'Migration aborted', null, 'ERROR');
-        return self::NOT_OK;
-      }
-      $this->logSection('propel', sprintf(
-        '%d of %d SQL statements executed successfully on datasource "%s"',
-        $res,
-        count($statements),
-        $datasource
-      ));
-      $manager->updateLatestMigrationTimestamp($datasource, $nextMigrationTimestamp);
-      if ($options['verbose'])
-      {
-        $this->logSection('propel', sprintf('  Updated latest migration date to %d for datasource "%s"', $nextMigrationTimestamp, $datasource), null, 'COMMENT');
-      }
-    }
 
-    $migration->postUp($manager);
+        $migration = $manager->getMigrationObject($migrationName);
+        if (false === $migration->preUp($manager))
+        {
+            $this->logSection('propel', 'preUp() returned false. Aborting migration.', null, 'ERROR');
+            return self::NOT_OK;
+        }
 
-    if ($timestamps = $manager->getValidMigrationTimestamps())
-    {
-      $this->logSection('propel', sprintf(
-        'Migration complete. %d migrations left to execute.',
-        count($timestamps)
-      ));
-    }
-    else
-    {
-      $this->logSection('propel', 'Migration complete. No further migration to execute.');
-    }
+        foreach ($migration->getUpSQL() as $datasource => $sql)
+        {
+            $connection = $manager->getConnection($datasource);
+            if ($options['verbose'])
+            {
+                $this->logSection('propel', sprintf('  Connecting to database "%s" using DSN "%s"', $datasource, $connection['dsn']), null, 'COMMENT');
+            }
+            $pdo = $manager->getPdoConnection($datasource);
+            $res = 0;
+            $statements = PropelSQLParser::parseString($sql);
+            foreach ($statements as $statement)
+            {
+                try
+                {
+                    if ($options['verbose'])
+                    {
+                        $this->logSection('propel', sprintf('  Executing statement "%s"', $statement), null, 'COMMENT');
+                    }
+                    $stmt = $pdo->prepare($statement);
+                    $stmt->execute();
+                    $res++;
+                }
+                catch (PDOException $e)
+                {
+                    $this->logSection(sprintf('Failed to execute SQL "%s". Aborting migration.', $statement), null, 'ERROR');
+                    return self::NOT_OK;
+                    // continue
+                }
+            }
+            if (!$res)
+            {
+                $this->logSection('propel', 'No statement was executed. The version was not updated.');
+                $this->logSection('propel', sprintf(
+                    'Please review the code in "%s"',
+                    $manager->getMigrationDir() . DIRECTORY_SEPARATOR . $migrationName
+                ));
+                $this->logSection('propel', 'Migration aborted', null, 'ERROR');
+                return self::NOT_OK;
+            }
+            $this->logSection('propel', sprintf(
+                '%d of %d SQL statements executed successfully on datasource "%s"',
+                $res,
+                count($statements),
+                $datasource
+            ));
+            $manager->addExecutedMigration($datasource, $migrationName);
+            if ($options['verbose'])
+            {
+                $this->logSection('propel', sprintf('  Added %s to executed migrations for datasource "%s"', $migrationName, $datasource), null, 'COMMENT');
+            }
+        }
 
-    return self::OK;
-  }
+        $migration->postUp($manager);
+
+        if (count($missingMigrations))
+        {
+            $this->logSection('propel', sprintf(
+                'Migration complete. %d migrations left to execute.',
+                count($missingMigrations)
+            ));
+        }
+        else
+        {
+            $this->logSection('propel', 'Migration complete. No further migration to execute.');
+        }
+
+        return self::OK;
+    }
 
 }
